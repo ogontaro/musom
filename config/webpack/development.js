b04988b78b7c3b45ff88781efdb63beb3ce25abc
ogontaro <ogontaro@gmail.com>
2018-06-09 21:57:42 +0900

    add eslint

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+environment.loaders.set('eslint', {
+  test: /\.(js|vue)$/,
+  loader: 'eslint-loader',
+  enforce: 'pre',
+  options: {
+    failOnWarning: true
+  }
+})
