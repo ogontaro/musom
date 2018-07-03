aeff8d4f2d9f8aeef1505d3dc220eb70dcdef015
ogontaro <ogontaro@gmail.com>
2018-07-03 23:36:59 +0900

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get :sign_up, to: 'registrations#new', as: :new_user_registration
+  post :users, to: 'registrations#create', as: :user_registration
 end
