2d2e668076f8f6aa9d0818946ab2117db469e344
ogontaro <ogontaro@gmail.com>
2018-07-08 19:53:27 +0900

    add new user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get :sign_up, to: 'registrations#new', as: :new_user_registration
-  post :users, to: 'registrations#create', as: :user_registration
+  get :sign_up, to: 'users#new', as: :new_users
+  resources :users, only: :create
 end
