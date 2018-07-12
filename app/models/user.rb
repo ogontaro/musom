204f582438d97fa1adfd32e35ed0912eaa2a373c
ogontaro <ogontaro@gmail.com>
2018-07-12 22:38:50 +0900

    remove password

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,5 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :providers
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, uniqueness: true
 end
