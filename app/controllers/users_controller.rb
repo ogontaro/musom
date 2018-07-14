18cf178d8ff25141a3aba7309bb3ebda34f5d188
ogontaro <ogontaro@gmail.com>
2018-07-14 20:59:01 +0900

    add activate routes controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def activate
+
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   get :sign_up, to: 'users#new', as: :new_users
+  get :activate, to: 'users#activate', as: :activate_user
   resources :users, only: :create
 end
